934a7c5ede2bfd076009a33e7d121e178928a59a
Rhys Previte <previterp88@gmail.com>
2020-10-08 16:21:30 -0500

    Project complete

--- mole.js
+++ mole.js
@@ -1,37 +1,30 @@
-// Write your JS here.
-// window.addEventListener('DOMContentLoaded', () => {
-
-//     setInterval(() => {
-//       const moleHeads = document.querySelectorAll('.wgs__mole-head');
-//       for (let moleHead of moleHeads) {
-//         moleHead.classList.toggle('wgs__mole-head--hidden');
-//       }
-//     }, 1000);
-
-//   });
-
 window.addEventListener('DOMContentLoaded', (e) => {
-    setTimeout(popUpRandomMole, 0)
-
-
-})
+    setTimeout(popUpRandomMole, 0);
+    const moleHead = document.querySelectorAll(".wgs__mole-head");
+    moleHead.forEach(mole => {
+        mole.addEventListener('click', e => {
+            mole.classList.add("wgs__mole-head--hidden");
+            mole.classList.add("wgs__mole-head--whacked");
+        })
+    })
+});
 
 function popUpRandomMole() {
-    const moleList = document.querySelectorAll(".wgs__mole-head");
-    const randomNum = Math.floor(Math.random() *  8);
+    const moleList = document.querySelectorAll(".wgs__mole-head:not(.wgs__mole-head--whacked)");
+    // console.log(moleList.length);
+    if (moleList.length === 0) {
+        alert("You won!");
+        return; 
+    } 
+    const randomNum = Math.floor(Math.random() *  moleList.length);
     const mole = moleList[randomNum];
     mole.classList.remove("wgs__mole-head--hidden");
     setTimeout(() => {
         hideMole(mole);
     }, 1000);
-}
+};
 
 function hideMole(element){
     element.classList.add("wgs__mole-head--hidden")
     setTimeout(popUpRandomMole, 1000);
-
-}
-
-
-
-// console.log(popUpRandomMole());
+};
