432f174ef5163cbc827ce7fc7980798a851c5ea4
dezadkins <desmondadkins@gmail.com>
2020-10-08 13:18:15 -0700

    step 2 done

--- mole.js
+++ mole.js
@@ -7,5 +7,32 @@
 //         moleHead.classList.toggle('wgs__mole-head--hidden');
 //       }
 //     }, 1000);
-  
+
 //   });
+
+window.addEventListener('DOMContentLoaded', (e) => {
+    // setTimeout(popUpRandomMole, 0)
+        popUpRandomMole();
+
+
+})
+
+function popUpRandomMole () {
+    const moleList = document.querySelectorAll(".wgs__mole-head");
+    const randomNum = Math.floor(Math.random() *  8);
+    const mole = moleList[randomNum];
+    mole.classList.remove("wgs__mole-head--hidden");
+    setTimeout(() => {
+        hideMole(mole);
+    }, 1000);
+}
+
+function hideMole(element){
+    element.classList.add("wgs__mole-head--hidden")
+    setTimeout(popUpRandomMole, 1000);
+
+}
+
+
+
+// console.log(popUpRandomMole());
